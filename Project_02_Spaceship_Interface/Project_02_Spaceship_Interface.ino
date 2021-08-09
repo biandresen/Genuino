4cdcacec619ce8eaa40c7467e0e5c13d6779db6b
biandresen <36762853+biandresen@users.noreply.github.com>
2021-08-09 22:50:02 +0200

    Add files via upload

--- Project_02_Spaceship_Interface/Project_02_Spaceship_Interface.ino
+++ Project_02_Spaceship_Interface/Project_02_Spaceship_Interface.ino
@@ -1,16 +1,29 @@
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(2,INPUT);
+//MATERIAL USED:               1 Genuino Uno, 
+//                             1 small breadboard
+//                             1 switch
+//                             3 LED(2 red, 1 green) 
+//                             Resistors (10kOhm for pulldown switch and 220Ohm for each LED)
+//
+//INTENTION OF THE PROJECT
+//  For IDLE:                 Green LED ON
+//  For switch activated:     Red LEDs alternate ON/OFF with 250ms interval
+//  For switch released:      Back to IDLE after 250ms
+
+ 
+//Runs once. Pin configuration/functionality 
+void setup() {                     
+  pinMode(2,INPUT); 
   pinMode(3,OUTPUT);
   pinMode(4,OUTPUT);
   pinMode(5,OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  int switchState = 0;
-  switchState = digitalRead(2);
+//Actuation of the LEDs by functions/statements
+void loop() {                     
+  int switchState = 0;            //Declaration of variable (integer)
+  switchState = digitalRead(2);   //Checking the state of pin 2
 
+//Setting the various LEDs high and low
   if (switchState == LOW) {
     digitalWrite(3, HIGH);
     digitalWrite(4, LOW);
